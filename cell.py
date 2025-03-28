bf33eb536c3809b271829423c299c764f1785f5b
Liyang-86 <177089640+Liyang-86@users.noreply.github.com>
2025-03-29 00:09:23 +0800

    add Maze class

--- cell.py
+++ cell.py
@@ -13,6 +13,8 @@ class Cell:
         self._win = win
     
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
@@ -29,3 +31,13 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        line_color = "red"
+        if undo:
+            line_color = "gray"
+        line = Line(
+            Point((self._x1 + self._x2) // 2, (self._y1 + self._y2) // 2),
+            Point((to_cell._x1 + to_cell._x2) // 2, (to_cell._y1 + to_cell._y2) // 2)
+            )
+        self._win.draw_line(line, line_color)
--- main.py
+++ main.py
@@ -3,22 +3,24 @@ from cell import Cell
 
 def main():
     win = Window(800, 600)
-    
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    c1 = Cell(win)
+    c1.has_left_wall = False
+    c1.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
+    c2 = Cell(win)
+    c2.has_right_wall = False
+    c2.draw(125, 125, 200, 200)
 
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
+    c3 = Cell(win)
+    c3.has_bottom_wall = False
+    c3.draw(225, 225, 250, 250)
+
+    c4 = Cell(win)
+    c4.has_top_wall = False
+    c4.draw(300, 300, 500, 500)
+
+    c1.draw_move(c2)
 
     win.wait_for_close()
 
